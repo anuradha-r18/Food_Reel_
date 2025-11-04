b477674cd3de86a2866e37bdaecfee3e4b823f3d
Anuradha <anuradharaghuwanshi033@gmail.com>
2025-11-04 23:14:07 +0530

    Initial commit - setup Food_Reel project structure

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authController = require("../controllers/auth.controller")
+
+const router = express.Router();
+
+ //user api's
+router.post('/user/register', authController.registerUser)
+router.post('/user/login', authController.loginUser)
+router.get('/user/logout' , authController.logoutUser)
+
+// foodpartner api's
+router.post('/food-partner/register' , authController.registerFoodPartner)
+router.post('/food-partner/login' , authController.loginFoodPartner)
+router.get('/food-partner/logout' , authController.logoutFoodPartner)
+
+
+
+module.exports = router;
---
+++ backend/src/routes/food.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const foodController = require('../controllers/food.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
+const router = express.Router();
+const multer = require('multer')
+
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+
+
+// method=POST  route="/api/food/"  [protected]
+router.post('/',authMiddleware.authFoodPartnerMiddleware ,upload.single("video") ,  foodController.createFood)
+
+
+// GET /api/food/   [proctected]
+router.get('/', authMiddleware.authUserMiddleware , foodController.getFoodItems )
+
+module.exports = router ;
